977c077014c5cb0a858b0ad2b3797fd0e5e90401
saidam90 <saida.murtazaliyeva@yahoo.com>
2024-05-11 20:07:27 +0400

    Revert "Solutions for 5.6 Blog List Frontend, step 6 and 5.7 Blog List Frontend, step 7"
    This reverts commit 482cfdba6fb0c2212bce0761f800361643edc25e.

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,39 +1,7 @@
-import { useState } from "react";
-
-const Blog = ({ blog }) => {
-  const [view, setView] = useState(false);
-
-  const blogStyle = {
-    paddingTop: 10,
-    paddingLeft: 2,
-    border: "solid",
-    borderWidth: 1,
-    marginBottom: 5,
-  };
-
-  return (
-    <div style={blogStyle}>
-      {!view ? (
-        <div>
-          {blog.title} {blog.author}
-          <button onClick={() => setView(true)}>view</button>
-        </div>
-      ) : (
-        <div>
-          <div>
-            {blog.title} {blog.author}
-            <button onClick={() => setView(false)}>hide</button>
-          </div>
-          <div>{blog.url}</div>
-          <div>
-            {blog.likes} likes
-            <button>like</button>
-          </div>
-          <div className="blog-user">{blog.user ? blog.user.name : ""}</div>
-        </div>
-      )}
-    </div>
-  );
-};
+const Blog = ({ blog }) => (
+  <div>
+    {blog.title} {blog.author}
+  </div>
+);
 
 export default Blog;
