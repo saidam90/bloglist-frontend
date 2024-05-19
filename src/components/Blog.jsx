3ac0e5b640cf6f54d8b9f6d24a6c58939ef41d6a
saidam90 <saida.murtazaliyeva@yahoo.com>
2024-05-19 19:02:29 +0400

    Revert "Solution for 5.15: Blog List Tests, step 3"
    This reverts commit 13b83618dfe8d949660c571f11cfab0d9a421f1b.

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -34,13 +34,6 @@ const Blog = ({ blog, user, addLike, removeBlog, toggleVisibilityHandler }) => {
     }
   };
 
-  const likeHandler = () => {
-    setInfo(!info);
-    if (likeHandler) {
-      toggleVisibilityHandler();
-    }
-  };
-
   return (
     <div style={blogStyle} className="blog" data-testid="blog">
       <div>
--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -40,27 +40,3 @@ test("the blog's URL and number of likes are shown when 'view' is clicked", asyn
   expect(screen.getByText(blog.url)).toBeInTheDocument();
   expect(screen.getByText(`${blog.likes} likes`)).toBeInTheDocument();
 });
-
-test("if the like button is clicked twice, the event handler is called twice", async () => {
-  const blog = {
-    title: "My story",
-    author: "Saida",
-    url: "medium.com",
-    likes: 5,
-  };
-
-  const mockAddLike = vi.fn();
-
-  render(<Blog blog={blog} addLike={mockAddLike} />);
-
-  const user = userEvent.setup();
-  const viewButton = screen.getByText("view");
-  await user.click(viewButton);
-
-  const likeButton = screen.getByText("like");
-  for (let i = 0; i < 2; i++) {
-    await user.click(likeButton);
-  }
-
-  expect(mockAddLike).toHaveBeenCalledTimes(2);
-});
